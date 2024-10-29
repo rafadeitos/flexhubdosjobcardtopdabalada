38d047ffb5d6e580ff8b98be7be7d7773932219e
rafadeitos <140817205+rafadeitos@users.noreply.github.com>
2024-10-29 08:43:37 -0300

    h

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'programacao',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação',
+)
+criaCartao(
+    'projeto vida',
+    'o que e a vida?',
+    'a vida é uma maravilha',
+)
+criaCartao(
+    'ed. financeira',
+    'qual a moeda da indonésia?',
+    'Rupia indonésia',
+)
+criaCartao(
+    'biologia 1',
+    'o que é genética?',
+    'é uma ciência que faz parte da biologia. Ela tem como objetivo compreender a transmissão das características dos seres vivos para seus descendentes,',
+)
+criaCartao(
+    'biología',
+    'o que é sucessão ecológica?',
+    'Sucessão ecológica é a sequência de comunidades, desde a colonização até à comunidade clímax, de um determinado ecossistema.',
+)
+criaCartao(
+    'química',
+    'o que é nox?',
+    'O estado de oxidação, também chamado de número de oxidação, é uma abordagem para atribuir o valor da carga que um átomo em uma substância possui.',
+)
+criaCartao(
+    'sociologia',
+    'quem foi karl marx?',
+    'Karl Marx foi um filósofo, economista, historiador, sociólogo, teórico político, jornalista, e revolucionário socialista alemão.',
+)
+criaCartao(
+    'ingles',
+    'que dia é hoje em ingles?',
+    'tuesday',
+)
+criaCartao(
+    'matemática 1',
+    ' o que é clt?',
+    'é a legislação trabalhista que apresenta as regras para uma relação de vínculo empregatício.',
+)
+criaCartao(
+    'história',
+    'quem foi hitler?',
+    'Adolf Hitler, foi um político alemão que serviu como líder do Partido Nazista, Chanceler do Reich e Führer da Alemanha Nazista de 1934 até 1945.',
+)
+criaCartao(
+    'fisica',
+    'O que é força empuxo?',
+    'força exercida por um fluido sobre um objeto mergulhado total ou parcialmente nele.',
+)
+criaCartao(
+    'geografia',
+    'O que são fontes de energia?',
+    'recursos naturais ou artificiais utilizados pela sociedade para produção de algum tipo de energia.',
+)
+criaCartao(
+    'português',
+    'O que é texto argumentativo?',
+    'é aquele que tem como principais características defender uma ideia, hipótese, teoria ou opinião. ',
+
+)
+criaCartao(
+    'matemática',
+    'O que é hipotenusa?',
+    'significa, em geometria, o lado de um triângulo retângulo, oposto ao ângulo reto.',
+    
